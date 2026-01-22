a445230ef5175bea1369ad4614fc3292e3968f30
beatrixwashere <turtlezdude@gmail.com>
2026-01-22 14:53:59 -0600

    add list and error functionality

---
+++ addons/whitecell/scripts/ui/error.gd
@@ -0,0 +1,7 @@
+@tool
+extends ColorRect
+## handles error completion
+
+
+func complete_error() -> void:
+	queue_free()
--- addons/whitecell/scripts/ui/list.gd
+++ addons/whitecell/scripts/ui/list.gd
@@ -1,5 +1,10 @@
-class_name ErrorList
+@tool
 extends ColorRect
 ## handles list customization and orgnaization
 
 @export var list_name: String = "list"
+
+
+func new_error() -> void:
+	var error_node: Node = load("res://addons/whitecell/scenes/error.tscn").instantiate()
+	$scroll/todo.add_child(error_node)
