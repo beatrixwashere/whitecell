b2094b6dfea5eafdbbc4edd05b1af226b4b1804d
beatrixwashere <turtlezdude@gmail.com>
2026-01-23 13:34:39 -0600

    add new functionality to lists

--- addons/whitecell/scripts/ui/list.gd
+++ addons/whitecell/scripts/ui/list.gd
@@ -2,9 +2,23 @@
 extends ColorRect
 ## handles list customization and orgnaization
 
-@export var list_name: String = "list"
 
-
-func new_error() -> void:
+func new_error(ename: String = "todo") -> void:
 	var error_node: Node = load("res://addons/whitecell/scenes/error.tscn").instantiate()
 	$scroll/todo.add_child(error_node)
+
+
+func delete_list() -> void:
+	queue_free()
+
+
+func change_color(clr: Color) -> void:
+	color = clr
+
+
+func toggle_color_picker(vis: bool) -> void:
+	$color/picker.visible = vis
+
+
+func move_list(diff: int) -> void:
+	get_parent().move_child(self, min(max(get_index() + diff, 0), get_parent().get_child_count() - 2))
