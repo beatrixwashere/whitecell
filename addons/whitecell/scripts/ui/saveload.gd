8ebd40cf87b4bd60f6b4c4b7bbfdfd694bce537b
beatrixwashere <turtlezdude@gmail.com>
2026-01-30 15:23:31 -0600

    save path can now be edited

--- addons/whitecell/scripts/ui/saveload.gd
+++ addons/whitecell/scripts/ui/saveload.gd
@@ -13,20 +13,20 @@ func save_lists() -> void:
 			for j in i.get_node("scroll/todo").get_children():
 				i_items.append(j.get_node("label_edit").text)
 			new_data.list_items.append(i_items)
-	DirAccess.remove_absolute("res://whitecell.tres")
-	new_data.take_over_path("res://whitecell.tres")
-	ResourceSaver.save(new_data, "res://whitecell.tres")
+	DirAccess.remove_absolute($savepath.text)
+	new_data.take_over_path($savepath.text)
+	ResourceSaver.save(new_data, $savepath.text)
 
 
 func load_lists() -> void:
-	if not FileAccess.file_exists("res://whitecell.tres"):
-		print("no save data found at res://whitecell.tres")
+	if not FileAccess.file_exists($savepath.text):
+		print("no save data found at " + $savepath.text)
 		return
 	for i in get_node("../listscroll/lists").get_children():
 		if i.name != "add":
 			i.queue_free()
 	var loaded_data: WhitecellData = ResourceLoader.load(
-			"res://whitecell.tres", "",
+			$savepath.text, "",
 			ResourceLoader.CACHE_MODE_IGNORE )
 	for i in loaded_data.list_names.size():
 		var i_list: Node = get_node("../listscroll/lists").new_list(loaded_data.list_names[i], true)
